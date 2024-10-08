b05460403bdf6130441d003cbcc8746b69743fdc
sa_allek <sa.alleksandr@gmail.com>
2024-10-08 11:49:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bookstore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookstore.ui.theme.BookStoreTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        val db = Firebase.firestore
+        db.collection("books").
+                document().set(mapOf("name" to "My fav book"))
+        }
+    }
+}
